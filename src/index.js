6880e0de2ff3f56adef4708aa3158d7c5556850f
Natalya Karpova <knataly0513@gmail.com>
2023-02-20 10:45:18 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,13 +6,16 @@ import { ThemeProvider } from '@emotion/react';
 import { theme } from './constants';
 import { Provider } from 'react-redux';
 import { store } from 'redux/store';
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </ThemeProvider>
     </Provider>
   </React.StrictMode >

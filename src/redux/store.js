685345aee3294e71b03930f52ff1f71c75306b03
Natalya Karpova <knataly0513@gmail.com>
2023-02-23 15:22:38 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,30 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+import { authInitState } from "./auth/auth.init-state";
+import { authReducer } from "./auth/auth.slice";
 
 
 import { formInitState } from "./contactForm/form.init-state";
 import { formReducer } from "./contactForm/form.slice";
 import { contactsInitState } from "./contactsFilter/contacts.init-state";
 import { contactsReducer } from "./contactsFilter/contacts.slice";
+import { profileInitState } from "./profile/profile.init-state";
+import { profileReducer } from "./profile/profile.slice";
 
 const initState = {
     form: formInitState,
     contacts: contactsInitState,
+    auth: authInitState,
+    profile: profileInitState,
 };
 
 export const store = configureStore({
@@ -16,12 +32,16 @@ export const store = configureStore({
     reducer: {
         form: formReducer,
         contacts: contactsReducer,
+        auth: authReducer,
+        profile: profileReducer,
     },
     devTools: true,
-    // middleware: getDefaultMiddleware =>
-    // getDefaultMiddleware({
-    //   serializableCheck: {
-    //     ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    //   },
-    // }),
+    middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);

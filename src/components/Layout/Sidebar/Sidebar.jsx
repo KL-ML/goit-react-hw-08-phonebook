0f44cf17b8faadc07963b7b6525a149d7334f2e1
Natalya Karpova <knataly0513@gmail.com>
2023-02-23 15:22:10 +0200

    Create Sidebar.jsx

---
+++ src/components/Layout/Sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import { Navigation } from "./Navigation/Navigation"; 
+
+export const Sidebar = () => {
+  return (
+    <aside>
+      <div>
+        <Navigation />
+      </div>
+    </aside>
+  );
+};

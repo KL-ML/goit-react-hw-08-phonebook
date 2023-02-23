58a00a26920f43ccf381ea3e76bb038cba79f45f
Natalya Karpova <knataly0513@gmail.com>
2023-02-23 15:22:28 +0200

    Create auth.slice.js

---
+++ src/redux/auth/auth.slice.js
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { STATUS } from 'constants/status.constants';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { getProfileThunk } from 'redux/profile/profile.thunk';
+
+import { authInitState } from './auth.init-state';
+import { authLoginThunk } from './auth.thunk';
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: authInitState,
+  reducers: {
+    logoutAction: () => authInitState,
+  },
+  extraReducers: builder => {
+    builder.addCase(authLoginThunk.pending, state => {
+      state.status = STATUS.loading;
+    }).addCase(authLoginThunk.fulfilled, (state, { payload }) => {
+      state.status = STATUS.success;
+      state.data = payload;
+    }).addCase(authLoginThunk.rejected, state => {
+      state.status = STATUS.error;
+    })
+      .addCase(getProfileThunk.rejected, () => authInitState);
+  },
+});
+
+export const { logoutAction } = authSlice.actions;
+
+export const authReducer = persistReducer({
+  key: 'auth',
+  storage,
+}, authSlice.reducer);

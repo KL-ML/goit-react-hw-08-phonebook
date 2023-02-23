a305636d5d97bfb75d64d1575425c0d7b93b46c9
Natalya Karpova <knataly0513@gmail.com>
2023-02-23 15:22:42 +0200

    Create LoginPage.jsx

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,79 @@
+import { STATUS } from 'constants/status.constants';
+import { useState } from 'react';
+
+import {
+    useDispatch,
+    useSelector
+} from 'react-redux';
+// import { useNavigate } from 'react-router-dom';
+import { selectAuthStatus } from 'redux/auth/auth.selector';
+import { authLoginThunk } from 'redux/auth/auth.thunk';
+
+const initialState = {
+  email: '',
+  password: '',
+};
+
+const LoginPage = () => {
+  const dispatch = useDispatch();
+  const status = useSelector(selectAuthStatus);
+
+  // const navigate = useNavigate();
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = event => {
+    const { value, name } = event.target;
+    setValues(prev => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+    try {
+      await dispatch(authLoginThunk(values)).unwrap();
+      console.log('Success');
+      // navigate('/', { replace: true });
+      //toast, або ще що завгодно
+    } catch {
+      console.log('Error');
+    }
+  };
+
+  return (
+    <>
+      {status === STATUS.loading && <p>Loading ...</p>}
+
+      <form action="#" onSubmit={handleSubmit}>
+        <h1>Login page</h1>
+
+        <div>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+            />
+          <label htmlFor="email">Email address</label>
+        </div>
+
+        <div>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={values.password}
+            onChange={handleChange}
+            />
+          <label htmlFor="password">Password</label>
+        </div>
+
+        <button type="submit">
+          Submit
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default LoginPage;

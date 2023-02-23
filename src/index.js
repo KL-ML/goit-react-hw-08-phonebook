787211e24c1b54531cf8d7cd2cd07473949467d1
Natalya Karpova <knataly0513@gmail.com>
2023-02-23 15:22:18 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,9 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
-        <BrowserRouter>
           <App />
-        </BrowserRouter>
       </ThemeProvider>
     </Provider>
   </React.StrictMode >

21362ecec92d5fe39ac334c12abe202cddb1cd91
Natalya Karpova <knataly0513@gmail.com>
2023-02-23 15:22:07 +0200

    Create Navigation.jsx

---
+++ src/components/Layout/Sidebar/Navigation/Navigation.jsx
@@ -0,0 +1,87 @@
+// import { useEffect } from 'react';
+
+// import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { NavLink, useLocation } from 'react-router-dom';
+import { selectAuthToken } from 'redux/auth/auth.selector';
+import { logoutAction } from 'redux/auth/auth.slice';
+import { selectProfileData } from 'redux/profile/profile.selector';
+import { getProfileThunk } from 'redux/profile/profile.thunk';
+
+// import { selectAuthToken } from '../../../../redux/auth/auth.selector';
+// import { logoutAction } from '../../../../redux/auth/auth.slice';
+// import { getProfileThunk } from '../../../../redux/profile/profile.thunk';
+// import { Button } from '../../../Button/Button';
+
+const getActiveClassName = ({ isActive }) => {
+  return isActive ? 'btn nav-btn btn-light active' : 'btn nav-btn btn-light';
+};
+
+export const Navigation = () => {
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  const token = useSelector(selectAuthToken);
+  const profile = useSelector(selectProfileData);
+
+  useEffect(() => {
+    if (token) {
+    dispatch(getProfileThunk());
+    }
+  }, [token, dispatch]);
+
+  return (
+    <div>
+      <div>
+              
+        {!token && <h2>Please log in!</h2>}
+
+        {token && profile && (
+          <>
+            <h2>Welcome!</h2>
+            <small>{profile.first_name} {profile.last_name}</small>
+            <small>{profile.email}</small>
+            <br />
+            <br />
+          </>
+        )}
+
+        <NavLink to="" className={getActiveClassName}>
+          Home page
+        </NavLink>
+        {/* <NavLink to="contacts" end className={getActiveClassName}>
+          Contacts list
+        </NavLink> */}
+
+        {token ? (
+          <>
+            <NavLink to="contacts" end className={getActiveClassName}>
+              Contacts list
+            </NavLink>
+
+            <NavLink to="new-contact" className={getActiveClassName}>
+              Create new contact
+            </NavLink>
+
+            <button
+              onClick={() => dispatch(logoutAction())}
+            >
+              Log Out
+            </button>
+          </>
+        ) : (
+          <>
+            <NavLink to="login" state={{ from: location }} className={getActiveClassName}>
+              Login
+            </NavLink>
+
+            <NavLink to="join" className={getActiveClassName}>
+              Join
+            </NavLink>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};

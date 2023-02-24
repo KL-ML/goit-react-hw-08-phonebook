8aaf678f764c04934601f558e59f57bbab722e10
Natalya Karpova <knataly0513@gmail.com>
2023-02-24 09:30:13 +0200

    Update auth.thunk.js

--- src/redux/auth/auth.thunk.js
+++ src/redux/auth/auth.thunk.js
@@ -1,9 +1,30 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-import { publicApi, token } from 'http/http'; 
+import { privateApi, publicApi, token } from 'http/http'; 
 
-export const authLoginThunk = createAsyncThunk('login', async (values) => {
-  const { data } = await publicApi.post('/users/login', values);
-  token.set(data);
-  return data;
-});
+export const authRegisterThunk = createAsyncThunk(
+  'register',
+  async (values) => {
+    const { data } = await publicApi.post('/users/signup', values);
+    token.set(data.token);
+    return data;
+  });
+
+export const authLoginThunk = createAsyncThunk(
+  'login',
+  async (values) => {
+    const { data } = await publicApi.post('/users/login', values);
+    token.set(data.token);
+    return data;
+  });
+
+export const authLogoutThunk = createAsyncThunk(
+  'logout',
+  async (_, thunkAPI) => {
+    try {
+      await privateApi.post('/users/logout');
+      token.remove();
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  });

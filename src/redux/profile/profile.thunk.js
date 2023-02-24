d27ee63d43b2aab975259b2a517b71af87b17c09
Natalya Karpova <knataly0513@gmail.com>
2023-02-24 21:28:59 +0200

    Update profile.thunk.js

--- src/redux/profile/profile.thunk.js
+++ src/redux/profile/profile.thunk.js
@@ -1,17 +1,21 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { privateApi, token } from "http/http";
 import { selectAuthToken } from "redux/auth/auth.selector";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 export const getProfileThunk = createAsyncThunk(
   'profile',
   async (_, { getState, rejectWithValue }) => {
-    const stateToken = selectAuthToken(getState()).token;
-
-    if (!stateToken) {
-      return rejectWithValue();
+    try {
+      const stateToken = selectAuthToken(getState()).token;
+      if (!stateToken) {
+        return rejectWithValue();
+      }
+      token.set(stateToken);
+      const { data } = await privateApi.get('/users/current');
+      return data;
+    } catch (e) {
+      Notify.failure(`${e.message}`);
+      return rejectWithValue(e.message);
     }
-
-    token.set(stateToken);
-    const { data } = await privateApi.get('/users/current');
-    return data;
   });

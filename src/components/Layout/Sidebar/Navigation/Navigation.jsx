10be103372abdadf4f9dad15d5359284f0692521
Natalya Karpova <knataly0513@gmail.com>
2023-02-24 09:52:36 +0200

    Update Navigation.jsx

--- src/components/Layout/Sidebar/Navigation/Navigation.jsx
+++ src/components/Layout/Sidebar/Navigation/Navigation.jsx
@@ -1,13 +1,10 @@
-// import { useEffect } from 'react';
-
-// import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink, useLocation } from 'react-router-dom';
 import { selectAuthToken } from 'redux/auth/auth.selector';
 import { logoutAction } from 'redux/auth/auth.slice';
-// import { selectProfileData } from 'redux/profile/profile.selector';
-// import { getProfileThunk } from 'redux/profile/profile.thunk';
+import { selectProfileData } from 'redux/profile/profile.selector';
+import { getProfileThunk } from 'redux/profile/profile.thunk';
 
 const getActiveClassName = ({ isActive }) => {
   return isActive ? 'btn nav-btn btn-light active' : 'btn nav-btn btn-light';
@@ -18,14 +15,14 @@ export const Navigation = () => {
   const location = useLocation();
 
   const token = useSelector(selectAuthToken);
-  // const profile = useSelector(selectProfileData);
+  const profile = useSelector(selectProfileData);
 
   useEffect(() => {
     if (token) {
       console.log('token at useEffect in Navigation:', token)
-    // dispatch(getProfileThunk());
+    dispatch(getProfileThunk());
     }
-  }, [token]);
+  }, [token, dispatch]);
 
   return (
     <div>
@@ -33,15 +30,14 @@ export const Navigation = () => {
               
         {!token && <h2>Please log in!</h2>}
 
-        {/* {token && profile && (
+        {token && profile && (
           <>
             <h2>Welcome!</h2>
-            <small>{profile.first_name} {profile.last_name}</small>
             <small>{profile.email}</small>
             <br />
             <br />
           </>
-        )} */}
+        )}
 
         <NavLink to="" className={getActiveClassName}>
           Home page

aa3386573702b453ec54fb6ab6a65cad2f6625b4
Natalya Karpova <knataly0513@gmail.com>
2023-02-24 10:52:42 +0200

    Update contacts.thunk.js

--- src/redux/contactsFilter/contacts.thunk.js
+++ src/redux/contactsFilter/contacts.thunk.js
@@ -1,42 +1,61 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from 'axios';
+import { privateApi, token } from "http/http";
+import { selectAuthToken } from "redux/auth/auth.selector";
 
 axios.defaults.baseURL = "https://63e10cb6dd7041cafb3fc6e4.mockapi.io";
 
 
 export const fetchContacts = createAsyncThunk(
     'contacts/fetchAll',
-    async (_, thunkAPI) => {
+    async (_, { getState, rejectWithValue }) => {
         try {
-            const { data } = await axios.get('/contacts');
+            const stateToken = selectAuthToken(getState()).token;
+
+            if (!stateToken) {
+                return rejectWithValue();
+            }
+
+            token.set(stateToken);
+            const { data } = await privateApi.get('/contacts');
             return data;
         } catch (e) {
-            return thunkAPI.rejectWithValue(e.message);
+            return rejectWithValue(e.message);
         }
     }
 );
 
 export const addContact = createAsyncThunk(
     'contacts/addContact',
-    async (currentContact, thunkAPI) => {
+    async (currentContact, { getState, rejectWithValue }) => {
         try {
             const { id, name, number } = currentContact;
-            const { data } = await axios.post('/contacts', { id, name, number });
+            const stateToken = selectAuthToken(getState()).token;
+            if (!stateToken) {
+                return rejectWithValue();
+            }
+            token.set(stateToken);
+            const { data } = await privateApi.post('/contacts', { id, name, number });
             return data;
         } catch (e) {
-            return thunkAPI.rejectWithValue(e.message);
+            return rejectWithValue(e.message);
         }
     }
 );
 
 export const deleteContact = createAsyncThunk(
     'contacts/deleteContact',
-    async (contactId, thunkAPI) => {
+    async (contactId, { getState, rejectWithValue }) => {
         try {
-            const { data } = await axios.delete(`/contacts/${contactId}`);
+            const stateToken = selectAuthToken(getState()).token;
+            if (!stateToken) {
+                return rejectWithValue();
+            }
+            token.set(stateToken);
+            const { data } = await privateApi.delete(`/contacts/${contactId}`);
             return data;
         } catch (e) {
-            return thunkAPI.rejectWithValue(e.message);
+            return rejectWithValue(e.message);
         }
     }
 );

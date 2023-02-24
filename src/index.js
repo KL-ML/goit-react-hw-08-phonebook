779ce4f5b9595acc3ae3fee2f7bd4db24d84787f
Natalya Karpova <knataly0513@gmail.com>
2023-02-24 09:30:06 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { ThemeProvider } from '@emotion/react';
 import { theme } from './constants';
 import { Provider } from 'react-redux';
 import { store } from 'redux/store';
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   

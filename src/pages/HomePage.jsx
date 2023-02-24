f461588b43e3a6a4e19620318d2b04009f2b4d9d
Natalya Karpova <knataly0513@gmail.com>
2023-02-24 19:20:14 +0200

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,27 +1,11 @@
 const HomePage = () => {
-  // const [trigger, { isLoading, data }] = useLazyGetPostsQuery();
-  // console.log(isLoading, data);
-
   return (
-    <>
-      <div>
-        <div>
-          <h1>Wellcome to your Phonebook</h1>
-          <p>
-            Using a series of utilities, you can create this jumbotron, just
-            like the one in previous versions of Bootstrap.
-          </p>
-          <button
-            // onClick={() => trigger({ page: 1 })}
-            type="button"
-          >
-            Marketing info
-          </button>
-        </div>
-      </div>
-
-      
-    </>
+    <div>
+      <h1>Wellcome to your Phonebook!</h1>
+      <p>
+        Save your contacts using this phonebook
+      </p>
+    </div>
   );
 };
 

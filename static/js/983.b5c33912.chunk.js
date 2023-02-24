36c8dfaf3ac2ed54040abc1d2256e8dfa9be80ad
KL-ML <103336477+KL-ML@users.noreply.github.com>
2023-02-24 18:40:37 +0000

    Deploying to gh-pages from @ KL-ML/goit-react-hw-08-phonebook@68bec2815c6bcdd2073ca682f59de9c9909dffba 🚀

---
+++ static/js/983.b5c33912.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[983],{3983:function(e,o,t){t.r(o);var n=t(184);o.default=function(){return(0,n.jsxs)("div",{children:[(0,n.jsx)("h1",{children:"Wellcome to your Phonebook!"}),(0,n.jsx)("p",{children:"Save your contacts using this phonebook"})]})}}}]);
+//# sourceMappingURL=983.b5c33912.chunk.js.map

0b031d162d561ae39159f096d8105925ed541094
Natalya Karpova <knataly0513@gmail.com>
2023-02-24 10:36:27 +0200

    Update profile.thunk.js

--- src/redux/profile/profile.thunk.js
+++ src/redux/profile/profile.thunk.js
@@ -2,14 +2,16 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { privateApi, token } from "http/http";
 import { selectAuthToken } from "redux/auth/auth.selector";
 
-export const getProfileThunk = createAsyncThunk('profile', async (_, { getState, rejectWithValue }) => {
-  const stateToken = selectAuthToken(getState()).token;
+export const getProfileThunk = createAsyncThunk(
+  'profile',
+  async (_, { getState, rejectWithValue }) => {
+    const stateToken = selectAuthToken(getState()).token;
 
-  if (!stateToken) {
-    return rejectWithValue();
-  }
+    if (!stateToken) {
+      return rejectWithValue();
+    }
 
-  token.set(stateToken);
-  const { data } = await privateApi.get('/users/current');
-  return data;
-});
+    token.set(stateToken);
+    const { data } = await privateApi.get('/users/current');
+    return data;
+  });

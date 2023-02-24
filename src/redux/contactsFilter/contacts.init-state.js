69450065eb847106871af5e68db15e8660d0deb6
Natalya Karpova <knataly0513@gmail.com>
2023-02-24 10:52:39 +0200

    Update contacts.init-state.js

--- src/redux/contactsFilter/contacts.init-state.js
+++ src/redux/contactsFilter/contacts.init-state.js
@@ -7,11 +7,3 @@ export const contactsInitState = {
         error: null,
     },
 };
-
-// Используй функцию createAsyncThunk для объявления асинхронный генераторов экшенов и выполнения HTTP-запросов. Обработку экшенов и изменение данных в состоянии Redux сделай при помощи createSlice.
-
-// Объяви следующие операции:
-
-// fetchContacts - получение массива контактов (метод GET) запросом. Базовый тип экшена "contacts/fetchAll".
-// addContact - добавление контакта (метод POST). Базовый тип экшена "contacts/addContact".
-// deleteContact - удаление контакта (метод DELETE). Базовый тип экшена "contacts/deleteContact".

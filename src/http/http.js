3c9a9bcb59e1a525746cea40dad1207f5a34ee6d
Natalya Karpova <knataly0513@gmail.com>
2023-02-24 09:30:04 +0200

    Update http.js

--- src/http/http.js
+++ src/http/http.js
@@ -1,20 +1,18 @@
 import axios from 'axios';
 
 export const publicApi = axios.create({
-  baseURL: 'https://taupe-croissant-c4162a.netlify.app/api',
-  // baseURL: 'https://connections-api.herokuapp.com/',
+  baseURL: 'https://connections-api.herokuapp.com',
   
 });
 
 export const privateApi = axios.create({
-  baseURL: 'https://taupe-croissant-c4162a.netlify.app/api',
-  // baseURL: 'https://connections-api.herokuapp.com/',
+  baseURL: 'https://connections-api.herokuapp.com',
   
 });
 
 export const token = {
-  set: (data) => {
-    privateApi.defaults.headers.Authorization = `${data.token_type} ${data.access_token}`;
+  set: (token) => {
+    privateApi.defaults.headers.Authorization = `Bearer ${token}`;
   },
 
   remove: () => {
